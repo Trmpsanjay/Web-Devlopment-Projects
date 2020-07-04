7846f1401ac13a41402df2afeec7482ee47ccfea
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-07-05 00:17:47 +0530

    Add files via upload

---
+++ Dice-Game/index.js
@@ -0,0 +1,13 @@
+
+var random1 = Math.floor(Math.random()*6)+1;
+var imagePath1 = "images/dice"+random1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",imagePath1,);
+var random2 = Math.floor(Math.random()*6)+1;
+var imagePath2 = "images/dice"+random2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",imagePath2,);
+if(random1>random2){
+  document.querySelector("h1").innerHTML="Player 1 Win";
+}
+else{
+  document.querySelector("h1").innerHTML="Player 2 Win";
+}
